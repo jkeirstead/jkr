41f88673cdd4d849030aae7862b999c3ece8a2b0
James Keirstead <james.keirstead@gmail.com>
2015-01-07 16:27:08 +0000

    Initial commit.

---
+++ R/jkeirstead-package.r
@@ -0,0 +1,12 @@
+#' jkeirstead
+#'
+#' This package provides convenient access to functions and variables
+#' that I commonly use for R analysis.  By putting these functions
+#' into their own package, it becomes easier to share code rather than
+#' passing around customized .Rprofile files and I can still
+#' automatically load everything but with a simpler
+#' \code{library(jkeirstead)} call.
+#'
+#' @name jkeirstead
+#' @docType package
+NULL
