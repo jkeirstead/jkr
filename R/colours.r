0a1f5e32325d9625f9e570facbff24bc02891b50
James Keirstead <james.keirstead@gmail.com>
2015-01-07 18:39:09 +0000

    Added some commonly used colours

---
+++ R/colours.r
@@ -0,0 +1,21 @@
+#' Colours
+#'
+#' Frequently used colour definitions.
+#'
+#' @return a list with the following defined colours:
+#' \itemize{
+#' \item \code{impblue} Imperial College dark blue
+#' \item \code{implightblue} Imperial College light blue
+#' \item \code{impbrown} complement of \code{impblue}
+#' \item \code{uesgreen} green of Urban Energy Systems project logo
+#' \item \code{bpyellow} yellow from BP logo
+#' \item \code{bpgreenyellow} green-yellow from BP logo
+#' }
+#' @export
+#' @name jkcolours
+jkcolours <- list(impblue=rgb(21,106,155,max=255),
+                uesgreen=rgb(34,177,76,max=255),
+                bpyellow=rgb(252,233,17,max=255),
+                bpgreenyellow=rgb(172,195,18,max=255),
+                implightblue=rgb(139,174,204,max=255),
+                impbrown=rgb(0.612,0.278,0.086))
